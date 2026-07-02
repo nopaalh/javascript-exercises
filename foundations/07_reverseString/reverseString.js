3bfa61f4415b44e2f474e6a5e92bfb139fc7c9d1
nopalh <nopalh25@gmail.com>
2026-07-02 15:24:19 +0700

    do number 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = str.split("").reverse().join("");
+    return newString;
 };
 
 // Do not edit below this line
