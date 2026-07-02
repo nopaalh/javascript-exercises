481be86eb745828e32acfa3b5c0e0757bb559c00
nopalh <nopalh25@gmail.com>
2026-07-02 16:46:27 +0700

    answering num 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+
+  return sum;
 	
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiSum = 1;
+  for (let i = 0; i < array.length; i++){
+    multiSum *= array[i];
+  }
+  return multiSum;
 };
 
-const power = function() {
+const power = function(a,b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let fact = 1;
+  if(a===0){
+    return 1;
+  }
+  
+  for (let i = 1; i <= a; i++){
+    fact *= i;
+  }
+  return fact;
 	
 };
 
