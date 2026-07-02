fc489e0515ed8057c1803c1c58f6c9cc59b850c4
nopalh <nopalh25@gmail.com>
2026-07-02 16:03:52 +0700

    Answering number 9, why js is so cunfusing wtff

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a < 0 || b < 0){
+        return "ERROR";
+    }else if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }else if(a > b){
+        for (let i = b; i <= a; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else{
 
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
