c97da703ea22eb6e6924b8a155f44842934758f2
nopalh <nopalh25@gmail.com>
2026-07-02 16:58:11 +0700

    answer num 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strNoPunct = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let strNoSpaces = strNoPunct.replaceAll(' ', '');
+
+    let left = 0;
+    let right = strNoSpaces.length - 1;
+
+    while (left < right) {
+        if (strNoSpaces[left] !== strNoSpaces[right]) {
+            return false;
+        }
+        left++;
+        right--;
+
+    }
+    return true;
+
 
 };
 
