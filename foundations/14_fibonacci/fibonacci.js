367631896b6ef9b683e9803f0aff075a2c0a9eac
nopalh <nopalh25@gmail.com>
2026-07-02 17:01:51 +0700

    answer num 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0){
+        return "OOPS";
+    }
+    let fib = [0, 1];
+    for (let i = 2; i <= a; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    return fib[a];
 
 };
 
